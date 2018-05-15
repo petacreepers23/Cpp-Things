9ff790b8a9b003a3ea3d789cbd403238216920fb
petacreepers23 <javiermiguel4b@gmail.com>
2018-05-15 10:29:31 +0200

    Added a very useful chrono
    Its emulating matlab tic() toc() and implemented with a high resolution
    clock

---
+++ TicToc.hpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <chrono>
+
+class TicToc {
+public:
+	void tic() {
+		t0 = std::chrono::high_resolution_clock::now();
+	}
+	void toc() {
+		t1 = std::chrono::high_resolution_clock::now();
+		std::cout << "Elapsed: " << 1.e-9*std::chrono::duration_cast<std::chrono::nanoseconds>(t1 - t0).count() << "s" << std::endl;
+	}
+private:
+	std::chrono::steady_clock::time_point t0;
+	std::chrono::steady_clock::time_point t1;
+};
