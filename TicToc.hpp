06dd418a170f4a936cfce651cd32b3cb050c9713
asm4ever <alejandro-perseo.gutierrez.izquierdo@alumnos.upm.es>
2019-01-25 12:39:48 +0100

    Cambio en toc

--- TicToc.hpp
+++ TicToc.hpp
@@ -10,6 +10,10 @@ public:
 		t1 = std::chrono::high_resolution_clock::now();
 		std::cout << "Elapsed: " << 1.e-9*std::chrono::duration_cast<std::chrono::nanoseconds>(t1 - t0).count() << "s" << std::endl;
 	}
+	void toc(int iters) {
+		t1 = std::chrono::high_resolution_clock::now();
+		std::cout << "Elapsed: " << 1.e-9*std::chrono::duration_cast<std::chrono::nanoseconds>(t1 - t0).count() / iters << "s" << std::endl;
+	}
 private:
 	std::chrono::steady_clock::time_point t0;
 	std::chrono::steady_clock::time_point t1;
