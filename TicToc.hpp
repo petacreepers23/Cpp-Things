190ed00f62fcf91e77726b89d5c1197ac0396f8e
petacreepers23 <javiermiguel4b@gmail.com>
2019-01-25 13:48:00 +0100

    Aniadidos comentarios
    Comments added

--- TicToc.hpp
+++ TicToc.hpp
@@ -3,13 +3,16 @@
 
 class TicToc {
 public:
+	//Init time counting
 	void tic() {
 		t0 = std::chrono::high_resolution_clock::now();
 	}
+	//End and print time counting
 	void toc() {
 		t1 = std::chrono::high_resolution_clock::now();
 		std::cout << "Elapsed: " << 1.e-9*std::chrono::duration_cast<std::chrono::nanoseconds>(t1 - t0).count() << "s" << std::endl;
 	}
+	//If geting time in a loop, divide time by its total iterations
 	void toc(int iters) {
 		t1 = std::chrono::high_resolution_clock::now();
 		std::cout << "Elapsed: " << 1.e-9*std::chrono::duration_cast<std::chrono::nanoseconds>(t1 - t0).count() / iters << "s" << std::endl;
