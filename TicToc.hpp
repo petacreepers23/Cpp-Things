046118d2a8969a41e48d07c0a3a0667a1a46538e
petacreepers23 <javiermiguel4b@gmail.com>
2020-12-09 21:31:49 +0100

    Añadido namespace P23 Quick update en TicToc Añadida funcion "split"

--- TicToc.hpp
+++ TicToc.hpp
@@ -1,23 +1,27 @@
 #include <iostream>
 #include <chrono>
 
-class TicToc {
-public:
-	//Init time counting
-	void tic() {
-		t0 = std::chrono::high_resolution_clock::now();
-	}
-	//End and print time counting
-	void toc() {
-		t1 = std::chrono::high_resolution_clock::now();
-		std::cout << "Elapsed: " << 1.e-9*std::chrono::duration_cast<std::chrono::nanoseconds>(t1 - t0).count() << "s" << std::endl;
-	}
-	//If geting time in a loop, divide time by its total iterations
-	void toc(int iters) {
-		t1 = std::chrono::high_resolution_clock::now();
-		std::cout << "Elapsed: " << 1.e-9*std::chrono::duration_cast<std::chrono::nanoseconds>(t1 - t0).count() / iters << "s" << std::endl;
-	}
-private:
-	std::chrono::steady_clock::time_point t0;
-	std::chrono::steady_clock::time_point t1;
-};
+//Petacreepers23 namespace
+namespace P23 {
+
+	class TicToc {
+	public:
+		//Empieza el contador
+		void tic() {
+			t0 = std::chrono::high_resolution_clock::now();
+		}
+		//Acaba e imprime el tiempo
+		void toc() {
+			t1 = std::chrono::high_resolution_clock::now();
+			std::cout << "Elapsed: " << 1.e-9*std::chrono::duration_cast<std::chrono::nanoseconds>(t1 - t0).count() << "s" << std::endl;
+		}
+		//Si se ha ejecutado en un bucle, divide el tiempo total entre el numero de iteraciones
+		void toc(size_t iters) {
+			t1 = std::chrono::high_resolution_clock::now();
+			std::cout << "Elapsed: " << 1.e-9*std::chrono::duration_cast<std::chrono::nanoseconds>(t1 - t0).count() / iters << "s" << std::endl;
+		}
+	private:
+		std::chrono::steady_clock::time_point t0;
+		std::chrono::steady_clock::time_point t1;
+	};
+}
