0e81190bef81bd9e1d6190f1025b0975aa166f90
Carlos M.J <45593218+krlsm@users.noreply.github.com>
2020-12-13 18:38:15 +0100

    Update AritmeticUtils.hpp
    identation

--- AritmeticUtils.hpp
+++ AritmeticUtils.hpp
@@ -1,9 +1,9 @@
 namespace P23 {
 /*Returns the modulus operation like in python: -90 mod 360 = 270*/
-template <typename T> 
-T mod_pythonic(T a , T b){
-	return (b +((a)%b))%b;
-}
+	template <typename T> 
+	T mod_pythonic(T a , T b){
+		return (b +((a)%b))%b;
+	}
 	
 	
 	
