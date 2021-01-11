d620397f6992d3bfeb95f9d6b56bfec5fbb79236
Perseo <alejandro-perseo.gutierrez.izquierdo@alumnos.upm.es>
2021-01-11 22:48:45 +0100

    added config spliting lines

---
+++ tests/configure.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -x
+
+sudo apt-get install libgtest-dev
+cd /usr/src/gtest
+sudo cmake CMakeLists.txt
+sudo make
+sudo cp *.a /usr/lib
+sudo ln -s /usr/lib/libgtest.a /usr/local/lib/libgtest.a
+sudo ln -s /usr/lib/libgtest_main.a /usr/local/lib/libgtest_main.a
